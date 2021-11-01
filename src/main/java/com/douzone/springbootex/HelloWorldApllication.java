e2b671c16e31e233a1536a899a00dbe03738b4f7
wjddn154 <gjwjddn154@gmail.com>
2021-11-01 11:22:08 +0900

    springboot-ex #1

---
+++ src/main/java/com/douzone/springbootex/HelloWorldApllication.java
@@ -0,0 +1,17 @@
+package com.douzone.springbootex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Bootstrap Class
+ *
+ */
+@SpringBootApplication //springboot basic dependency
+public class HelloWorldApllication {
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApllication.class, args);
+		
+	}
+
+}
